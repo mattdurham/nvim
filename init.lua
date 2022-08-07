faf3bcf813107e6cd3e32392e26619c62735c9dc
Matt Durham <mattdurham@ppog.org>
2022-08-07 19:59:36 -0400

    getting closer

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 require('plugins')
-require('lsp_config')
+--require('lsp_config')
 require('treesitter')
 require('tele')
 require('lualine').setup()
-require("go.format").goimport()  -- goimport + gofmt
---vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+--require("go.format").goimport()  -- goimport + gofmt
+vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
 vim.wo.colorcolumn = '240'
 vim.opt.autoindent = true
 vim.opt.cursorline = true
@@ -20,4 +20,9 @@ vim.cmd("let g:aurora_italic = 1 ")
 vim.cmd("let g:aurora_bold = 1")
 vim.cmd("colorscheme aurora")
 vim.cmd("let NERDTreeShowHidden=1")
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', 'ff', "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+vim.keymap.set({'n','i'}, '<F12>', vim.lsp.buf.definition, opts)
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,26 +6,138 @@ require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
-  use 'preservim/nerdtree'
+  use({
+    'ray-x/navigator.lua',
+    requires = {"ray-x/guihua.lua", run = "cd lua/fzy && make" },
+        config = function()
+          require("navigator").setup({
+            debug = true,
+            lsp = {
+              format_on_save = true,
+              diagnostic_scrollbar_sign = { "▃", "▆", "█" }, -- set to nil to disable, set to {'╍', 'ﮆ'} to enable diagnostic status in scroll bar area
+            },
+          })
+        end,
+  })
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use ({
+    'ray-x/go.nvim',
+    require('go').setup({
+      run_in_floaterm = true,
+      gopls_cmd = { 'gopls' },
+      icons = true,
+      verbose = false,
+      lsp_codelens = false,
+      lsp_keymaps = false,
+      lsp_diag_hdlr = false,
+      dap_debug_keymap = false,
+      textobjects = true,
+      luasnip = true,
+    })
+  })
+  use ({
+    'hrsh7th/nvim-cmp',
+    requires = {
+      "hrsh7th/cmp-nvim-lsp",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+
+        mapping = cmp.mapping.preset.insert({
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),  
+        }),
+
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+        },
+      })
+      require("cmp").setup.cmdline(":", {
+        sources = {
+          { name = "cmdline" },
+        },
+      })
+      require("cmp").setup.cmdline("/", {
+        sources = {
+          { name = "buffer" },
+        },
+      })
+    end,
+  })
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "python", "go", "javascript", "rust" },
+        highlight = { enable = true },
+      })
+    end,
+    run = ":TSUpdate",
+  })
+  use({
+    'ray-x/lsp_signature.nvim',
+    config = function()
+      local signature_config = {
+        log_path = vim.fn.expand("$HOME") .. "/tmp/sig.log",
+        debug = true,
+        verbose = true,
+        floating_window = false,
+        hint_enable = true,
+        handler_opts = {
+          border = "single",
+        },
+        max_width = 80,
+      }
+      require("lsp_signature").setup(signature_config)
+    end,
+  })
+
+  --use 'preservim/nerdtree'
   use 'junegunn/fzf.vim' 
   use 'junegunn/fzf'
   use 'kosayoda/nvim-lightbulb'
-  use 'tomasiser/vim-code-dark'
-  use 'jacoborus/tender.vim'
-  use 'nvim-treesitter/nvim-treesitter'
   use {
 	'nvim-telescope/telescope.nvim',
 	requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  --use 'hrsh7th/cmp-buffer'
+  --use 'hrsh7th/cmp-path'
+  --use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'theniceboy/vim-calc'
+  --use 'saadparwaiz1/cmp_luasnip'
   use 'ray-x/aurora'
   -- use 'ray-x/lsp_signature.nvim'
   use {
@@ -39,14 +151,18 @@ require('packer').startup(function()
     }
     end
   }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use {
-    'ray-x/go.nvim',
-    branch = "master"
-  }
-  use 'ray-x/guihua.lua' 
-  use 'ray-x/navigator.lua'
+   use {
+     'nvim-lualine/lualine.nvim',
+     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+   }
+   use {
+     "nvim-neo-tree/neo-tree.nvim",
+      requires = { 
+         "nvim-lua/plenary.nvim",
+         "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+         "MunifTanjim/nui.nvim",
+       },
+     }
+   
 end)
+
